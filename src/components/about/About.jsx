906916fee07205af4603da431932c676fdea0d0f
faizi2500 <faizan.zahid2500@gmail.com>
2022-07-04 19:25:54 +0100

    Add About section and Experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
-import Me from '../../assets/me1.png'
+import './about.css'
+import Me from '../../assets/me-bg.jpg'
+import Me1 from '../../assets/me1.png'
 import { FaAward, FaBlogger } from 'react-icons/fa'
-import { GrProjects } from 'react-icons/gr'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" className='about-section'>
       <h5>Get to know</h5>
       <h2>About Me</h2>
 
       <div className='container about-container'>
         <div className="image-section">
-          <img src={Me} alt="my-about-display" />
+          <img src={Me1} alt="my-about-display" className='image-me' />
         </div>
 
         <div className="abount-content">
           <div className="about-cards">
-            <article>
-              <FaAward />
+            <article className='article-card'>
+              <FaAward className='icon-size'/>
               <h3>Experience</h3>
-              <small>1 Year Working</small>
+              <small>0-1 Year Working</small>
             </article>
-            <article>
-              <FaBlogger />
+            <article className='article-card'>
+              <FaBlogger className='icon-size'/>
               <h3>Blogging</h3>
               <small>1+ Published</small>
             </article>
-            <article>
-              <GrProjects />
+            <article className='article-card'>
+              <VscFolderLibrary className='icon-size' />
               <h3>Projects</h3>
               <small>5+ Completed</small>
             </article>
           </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi corrupti aperiam ut odit aut vitae officiis fuga delectus, explicabo est atque voluptatum illo blanditiis quaerat recusandae sed reiciendis sequi quidem rem earum veritatis nisi qui nesciunt! Quisquam dignissimos ex fuga possimus expedita totam officiis iusto ratione architecto quam magni nostrum perspiciatis obcaecati unde, minima esse provident quod illum? Optio explicabo quam dolores sequi laborum tenetur dolorem quia nam atque quo expedita molestias laudantium, enim dicta.</p>
-          <a href="#">Let's Talk</a>
+          <p className='para-section'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi corrupti aperiam ut odit aut vitae officiis fuga delectus, explicabo est atque voluptatum illo blanditiis quaerat recusandae sed reiciendis sequi quidem rem earum veritatis nisi qui nesciunt! Quisquam dignissimos ex fuga possimus expedita totam officiis iusto ratione architecto quam magni nostrum perspiciatis obcaecati unde, minima esse provident</p>
+          <a href="#" id="btn-about-section" className='btn btn-primary about-btn'>Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { HiBadgeCheck } from 'react-icons/hi'
+import './experience.css'
 
 const Experience = () => {
+  const frontendSkills = ['HTML', 'CSS', 'BootStrap', 'TailWind', 'JavaScript', 'React', 'Jest', 'React Testing Library']
+  const backendSkills = ['Ruby', 'RoR', 'ExpressJS', 'PostgreSQL', 'MongoDB', 'ORMs']
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>  
+      <h2>My Experience</h2>
+
+      <div className='experiece-article-section'>
+        <article className='each-section-article'>
+          <h3 className='heading-frontend'>Frontend Development</h3>
+          <div className='frontend-skill-section'>
+            {frontendSkills.map((skill) => {
+              return (
+                <div className='each-skill'>
+                  <HiBadgeCheck />
+                  <h3>{skill}</h3>
+                </div>
+              )
+            })}
+          </div>
+        </article>
+        <aritcle className="each-section-article backend-skill-article">
+          <h3 className='heading-frontend'>Backend Development</h3>
+          <div className='frontend-skill-section'>
+            {backendSkills.map((skill) => {
+              return (
+                <div className='each-skill'>
+                  <HiBadgeCheck />
+                  <h3>{skill}</h3>
+                </div>
+              )
+            })}
+          </div>
+        </aritcle>
+      </div>
+    </section>
   )
 }
 
