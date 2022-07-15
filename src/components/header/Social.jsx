9191222003fe288cf0b2a4b272d88d7cb17996cd
faizi2500 <faizan.zahid2500@gmail.com>
2022-07-15 21:43:23 +0100

    Fix the linkedIn link

--- src/components/header/Social.jsx
+++ src/components/header/Social.jsx
@@ -6,13 +6,37 @@ import  { SiHashnode } from 'react-icons/si';
 const Social = () => {
   return (
     <div className="header-socials">
-      <a href="www.linkedin.com/in/faizan2500" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
-      <a href="https://github.com/faizi2500" target="_blank" rel="noopener noreferrer"><BsGithub/></a>
-      <a href="https://twitter.com/codewithfaizi" target="_blank" rel="noopener noreferrer"><BsTwitter/></a>
-      <a href="https://codewithfaizi.hashnode.dev/" target="_blank" rel="noopener noreferrer"><SiHashnode/></a>
-      <hr/>
+      <a
+        href="https://www.linkedin.com/in/faizan2500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/faizi2500"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsGithub />
+      </a>
+      <a
+        href="https://twitter.com/codewithfaizi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BsTwitter />
+      </a>
+      <a
+        href="https://codewithfaizi.hashnode.dev/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <SiHashnode />
+      </a>
+      <hr />
     </div>
-  )
+  );
 }
 
 export default Social
